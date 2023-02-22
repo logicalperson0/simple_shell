bc3e3c58bd31590b66c67f7ff3936cf345cb6d0d
Samuel <a2jsamuel@gmail.com>
2023-02-22 09:54:22 +0000

    fix

--- token.c
+++ token.c
@@ -10,16 +10,16 @@
 char **token(char *buff, char *delim)
 {
 	int i = 0, ac = 0;
-	char *buff_cpy;
-	char *str;
-	char **buffi;
+	char *buff_cpy = NULL;
+	char *str = NULL;
+	char **buffi = NULL;
 
 	buff_cpy = _strdup(buff);
 	str = strtok(buff, delim);
 
 	while (str)
 	{
-		str = strtok(NULL, " \n");
+		str = strtok(NULL, delim);
 		ac++;
 	}
 
@@ -28,12 +28,12 @@ char **token(char *buff, char *delim)
 	{
 		return (NULL);
 	}
-	str = strtok(buff_cpy, " \n");
+	str = strtok(buff_cpy, delim);
 
 	while (str)
 	{
 		buffi[i] = _strdup(str);
-		str = strtok(NULL, " \n");
+		str = strtok(NULL, delim);
 		i++;
 	}
 	buffi[i] = NULL;
